4719e10490de6f8373877140466454c3073f48c3
mutta0916 <mutta0916@gmail.com>
2021-09-08 21:41:43 +0900

    フォーマット修正

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# TryAws
+# README
+
+## コマンド
+
+package.json作成。プロジェクト作成時に実行。
+```
+$ npm init
+```
+
+node_modulesのインストール。
+```
+$ npm install
+```
+
+aws-sdkのインストール。
+（package.jsonに追記される。）
+ちなみにnpmのドキュメントでコマンドがわかる。
+```
+npm i aws-sdk
+```
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const AWS = require('aws-sdk');
-const fs = require('fs');
-const path = require('path');
+const AWS = require("aws-sdk");
+const fs = require("fs");
+const path = require("path");
 
 const s3 = new AWS.S3();
 
@@ -14,17 +14,17 @@ const s3 = new AWS.S3();
 // });
 
 var params = {
-  Bucket: "muttaso-bucket", 
-  Key: "muttaso.JPG"
- };
+  Bucket: "muttaso-bucket",
+  Key: "muttaso.JPG",
+};
 
-s3.getObject(params, function(err, data) {
-  if (err) console.log(err, err.stack); // an error occurred
-  else     console.log(data);           // successful response
+s3.getObject(params, function (err, data) {
+  if (err) console.log(err, err.stack);// an error occurred
+  else console.log(data); // successful response
   var writer = fs.createWriteStream(path.join(__dirname, "muttaso.JPG"));
   writer.on("finish", () => {
     console.log("success");
-  })
+  });
   writer.write(data.Body);
   writer.end();
 });
