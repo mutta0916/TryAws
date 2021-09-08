9d0c4d6863cea7b5332f9af216fda3add4a4b330
mutta0916 <mutta0916@gmail.com>
2021-09-08 21:21:43 +0900

    画像のダウンロード

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const AWS = require('aws-sdk');
+const fs = require('fs');
+const path = require('path');
+
+const s3 = new AWS.S3();
+
+// const params = {
+//     Bucket: 'muttaso-bucket', /* required */
+// };
+
+// s3.listObjects(params, function(err, data) {
+//     if (err) console.log(err, err.stack); // an error occurred
+//     else     console.log(data);           // successful response
+// });
+
+var params = {
+  Bucket: "muttaso-bucket", 
+  Key: "muttaso.JPG"
+ };
+
+s3.getObject(params, function(err, data) {
+  if (err) console.log(err, err.stack); // an error occurred
+  else     console.log(data);           // successful response
+  var writer = fs.createWriteStream(path.join(__dirname, "muttaso.JPG"));
+  writer.on("finish", () => {
+    console.log("success");
+  })
+  writer.write(data.Body);
+  writer.end();
+});
